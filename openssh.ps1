b663b1ce01466a491548573a78042bbf6e2a06c3
Ezzio Moreira <ezzio@infobytece.com.br>
2019-03-08 21:57:24 -0300

    Update openssh.ps1

--- openssh.ps1
+++ openssh.ps1
@@ -1,6 +1,5 @@
 choco install openssh -y
-cd 'C:\Program Files\OpenSSH-Win64'
+cd 'C:\Program Files\OpenSSH-Win32'
 .\install-sshd.ps1
-New-NetFirewallRule -DisplayName "OpenSSH Server (sshd)" -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
 Start-Service sshd
 Set-Service sshd -StartupType Automatic
