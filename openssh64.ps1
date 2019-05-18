fe69ae58935ffcff3ed5257e2b81f3e80576fbde
Ezzio Moreira <ezzio@infobytece.com.br>
2019-05-18 15:08:16 -0300

    Update openssh64.ps1

--- openssh64.ps1
+++ openssh64.ps1
@@ -3,3 +3,4 @@ cd 'C:\Program Files\OpenSSH-Win64'
 .\install-sshd.ps1
 Start-Service sshd
 Set-Service sshd -StartupType Automatic
+New-NetFirewallRule -DisplayName “Allow OpenSSH” -Direction Inbound -Program "C:\Program Files\OpenSSH-Win64\sshd.exe" -RemoteAddress LocalSubnet -Action Allow –Group “OpenSSH”
