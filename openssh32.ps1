c5f679d5af513b892f8cf161880287b268256347
Ezzio Moreira <ezzio@infobytece.com.br>
2019-11-17 10:32:58 -0300

    Delete openssh32.ps1

--- openssh32.ps1
+++
@@ -1,6 +0,0 @@
-choco install openssh -y
-cd 'C:\Program Files\OpenSSH-Win32'
-.\install-sshd.ps1
-Start-Service sshd
-Set-Service sshd -StartupType Automatic
-New-NetFirewallRule -DisplayName “Allow OpenSSH” -Direction Inbound -Program "C:\Program Files\OpenSSH-Win32\sshd.exe" -RemoteAddress LocalSubnet -Action Allow –Group “OpenSSH”
